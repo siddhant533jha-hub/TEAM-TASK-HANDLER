e2e97bb19c04ea65755cdca5e9fa3b899ed94e74
siddhant533jha-hub <siddhant533jha@gmail.com>
2026-05-27 19:42:33 +0530

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+ 
+export default MyApp;
